22c794922f7e64696c03588b313c415a59f157f5
Lavinia Tinca <lavinia_tinca@yahoo.com>
2023-10-12 23:19:55 +0300

    deleted unnecessary files

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import JoinPage from './pages/JoinPage';
 import Layout from './pages/Layout';
-import './App.css';
-
 
 const App = () => {
   return (
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
 import theme from './theme'; 
-import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
